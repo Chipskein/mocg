7b158a3430385b8757cb492505b6aead5a4e2032
Chipskein <abfn0905@gmail.com>
2022-08-21 20:52:21 -0300

    remoning exports

--- internals/config/config.go
+++ internals/config/config.go
@@ -13,14 +13,14 @@ type Config struct {
 	REMOTE_DIRECTORY_TYPE string //AWS || GDRIVE || none
 }
 
-var DefaultConfig = Config{REMOTE_DIRECTORY: false, LOCAL_DIRECTORY: true, DEFAULT_DIRECTORY: "", REMOTE_DIRECTORY_TYPE: "none"}
+var defaultConfig = Config{REMOTE_DIRECTORY: false, LOCAL_DIRECTORY: true, DEFAULT_DIRECTORY: "", REMOTE_DIRECTORY_TYPE: "none"}
 var UserConfig *Config
 
 func LoadConfigEnviroment() {
 	err := godotenv.Load("mocg.cfg")
 	if err != nil {
 		fmt.Println("Could not load config file using default")
-		UserConfig = &DefaultConfig
+		UserConfig = &defaultConfig
 	}
 
 }
