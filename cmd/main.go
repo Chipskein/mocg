a792ce6325bdfb8fe9600066f016bfafbe02286c
Chipskein <abfn0905@gmail.com>
2022-08-27 22:31:22 -0300

    arrumando go mod

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,5 @@
 package main
 
 func main() {
-	/*
-	   err := tb.Init()
 
-	   	if err != nil {
-	   		log.Fatal(err)
-	   	}
-
-	   defer tb.Close()
-	   var file = "/home/chipskein/Music/Prophecies.ogg"
-
-	   	for {
-	   		event := tb.PollEvent()
-	   		switch {
-	   		case event.Ch == ',':
-	   			//player.VolumeDown()
-	   		case event.Ch == '.':
-	   			//player.VolumeUp()
-	   		case event.Key == tb.KeyEsc:
-	   			os.Exit(0)
-	   		case event.Key == tb.KeySpace:
-	   			//player.PauseOrResume()
-	   		case event.Key == tb.KeyEnter:
-
-	   		case event.Key == tb.KeyArrowUp:
-	   			file = "/home/chipskein/Music/The Perfect Girl.ogg"
-	   		}
-	   	}
-	*/
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.14
 require (
 	github.com/faiface/beep v1.1.0
 	github.com/joho/godotenv v1.4.0
-	github.com/rivo/tview v0.0.0-20220812085834-0e6b21a48e96
 )
 
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
