e5133d99f1885d1f91f6fe1bb3a97f95ab5eb959
Chipskein <abfn0905@gmail.com>
2022-09-03 16:26:06 -0300

    adding cobra-cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"github.com/chipskein/mocg/internals/ui"
+	"fmt"
+
+	"github.com/spf13/cobra"
 )
 
+func Init() *cobra.Command {
+	return &cobra.Command{
+		Use:   "hello [name]",
+		Short: "retorna Olá + name passado",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Olá %s\n", args[0])
+		},
+	}
+}
+func Restore() *cobra.Command {
+	return &cobra.Command{
+		Use:   "hello [name]",
+		Short: "retorna Olá + name passado",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Olá %s\n", args[0])
+		},
+	}
+}
+func Kill() *cobra.Command {
+	return &cobra.Command{
+		Use:   "hello [name]",
+		Short: "retorna Olá + name passado",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Olá %s\n", args[0])
+		},
+	}
+}
 func main() {
-	ui.StartUI()
+	rootCmd := cobra.Command{}
+	rootCmd.AddCommand(Init(), Restore(), Kill())
+	rootCmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -5,12 +5,14 @@ go 1.17
 require (
 	github.com/faiface/beep v1.1.0
 	github.com/gizak/termui/v3 v3.1.0
+	github.com/spf13/cobra v1.5.0
 )
 
 require (
 	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
 	github.com/hajimehoshi/oto v0.7.1 // indirect
 	github.com/icza/bitio v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/jfreymuth/oggvorbis v1.0.1 // indirect
 	github.com/jfreymuth/vorbis v1.0.0 // indirect
 	github.com/mattn/go-runewidth v0.0.4 // indirect
@@ -19,6 +21,7 @@ require (
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
